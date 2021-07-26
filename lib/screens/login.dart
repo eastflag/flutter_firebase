30a6da76898483c1b26333b0602a78d476f42d22
eastflag <eastflag@gmail.com>
2021-07-27 03:03:44 +0900

    .

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AuthPage extends StatelessWidget {
-  const AuthPage({Key? key}) : super(key: key);
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -14,25 +16,90 @@ class AuthPage extends StatelessWidget {
         body: Stack(
           alignment: Alignment.center,
           children: [
-            Container(color: Colors.red),
+            Container(color: Colors.white),
             Column(
               mainAxisAlignment: MainAxisAlignment.end,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Container(width: 200, height: 200, color: Colors.amber),
+                // Image.asset("assets/images/loading.jpg"),
                 Stack(
                   children: [
-                    Container(width: 200, height: 200, color: Colors.cyan),
-                    Container(width: 100, height: 100, color: Colors.black),
+                    _inputForm(size),
+                    _authButton(size),
+                    // Container(width: 100, height: 50, color: Colors.black),
                   ],
                 ),
-                Container(height: size.height * 0.1),
+                Container(height: size.height * 0.08),
                 Text('Sign Up'),
                 Container(height: size.height * 0.05),
               ],
             )
           ],
         )
+    );
+  }
+
+  Widget _authButton(Size size) {
+    return Positioned(
+      left: size.width * 0.15,
+      right: size.width * 0.15,
+      bottom: 0,
+      child: RaisedButton(
+        child: Text("Login"),
+        color: Colors.blue,
+        onPressed: () => {},
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+  }
+
+  Widget _inputForm(Size size) {
+    return Padding(
+      padding: EdgeInsets.all(size.width*0.025),
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        elevation: 6,
+        child: Padding(
+          padding: EdgeInsets.only(left: 12.0, right: 12.0, top: 16.0, bottom: 23.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.account_circle),
+                    labelText: "Email",
+                  ),
+                  validator: (value) {
+                    if(value!.isEmpty) {
+                      return "Please input correct Email.";
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.vpn_key),
+                    labelText: "Password",
+                  ),
+                  validator: (value) {
+                    if(value!.isEmpty) {
+                      return "Please input correct Password.";
+                    }
+                    return null;
+                  },
+                ),
+                Container(height: 18,),
+                Text("Forgot Password"),
+              ],
+            ),),
+        ),
+      ),
     );
   }
 }
